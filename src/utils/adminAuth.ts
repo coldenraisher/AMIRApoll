f73c4a69f61fc804ae15f368e1df067eaa5c5520
coldenraisher <craisher@gmail.com>
2026-02-24 13:11:24 -0500

    Delete src directory

--- src/utils/adminAuth.ts
+++
@@ -1,6 +0,0 @@
-export const ADMIN_AUTH_KEY = "pollAdminUnlocked";
-
-export function isAdminUnlocked(): boolean {
-  if (typeof window === "undefined") return false;
-  return sessionStorage.getItem(ADMIN_AUTH_KEY) === "true";
-}
